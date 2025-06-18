46b3401a26e97cd4f6833a610b9389f7a1b7e5cc
BhautikMangukiya <bhautikmangukiya3386@gmail.com>
2025-06-18 16:53:06 +0530

    Fix missing createDefaultAdmin module for Render

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
-const createDefaultAdmin = require("./utils/createDefaultAdmin");
+const createDefaultAdmin = require("./Utils/createDefaultAdmin")
 
 dotenv.config();
 
